5abc08046ab6e208715a926ba1d4adb050e995ee
Naitive13 <hei.titan.2@gmail.com>
2025-07-31 18:21:18 +0300

    feat: use waitress to start api server

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -9,4 +10,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=8080)
